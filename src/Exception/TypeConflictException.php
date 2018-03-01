ce051f946da010fc8bec730c4067576ad454977d
Necip Allef <necipallef@gmail.com>
2018-03-01 13:19:21 +0300

    ADD TypeConflictException class

---
+++ src/Exception/TypeConflictException.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Fabs\Component\Serializer\Exception;
+
+
+use Fabs\Component\Assert\AssertionExceptionInterface;
+
+class TypeConflictException extends Exception implements AssertionExceptionInterface
+{
+
+}
